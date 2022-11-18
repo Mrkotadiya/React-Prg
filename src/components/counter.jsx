bfdaf45b0e7792b28cd8e9ae73563b2cc0982257
Mrkotadiya <nirbhayk7281@gmail.com>
2022-11-18 10:10:14 +0530

    Binding Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,30 +3,35 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags:['tag1','tag2','tag3']
   };
-  style={
-      fontWight:"bold",
-      fontSize:30
 
+  constructor(){
+    super();
+    this.handalIncrement = this.handalIncrement.bind(this)
+  }
+
+  handalIncrement() {
+    console.log("increment Clicked!", this);
   }
   render() {
     return (
       <div>
-        <span  className={this.getBageClasses()}>{this.formateData()}</span>
+        <span className={this.getBageClasses()}>{this.formateData()}</span>
 
-        <button className="btn btn-secondary btn-m">Increment </button>
-        <ul>
-            {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-        </ul>
+        <button
+          onClick={this.handalIncrement}
+          className="btn btn-secondary btn-m"
+        >
+          Increment{" "}
+        </button>
       </div>
     );
   }
-    getBageClasses() {
-        let classes = "btn m-2 btn-";
-        classes += (this.state.count) === 0 ? "warning" : "primary";
-        return classes;
-    }
+  getBageClasses() {
+    let classes = "btn m-2 btn-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
   formateData() {
     const { count } = this.state;
